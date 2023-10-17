912d3d5e397b3aefbdffe069737a19385b678612
haruki7049 <tontonkirikiri@gmail.com>
2023-10-17 15:31:12 +0900

    deleted evaluator

--- src/evaluator/mod.rs
+++
@@ -1,6 +0,0 @@
-use crate::data::token::SeparatedToken;
-use crate::data::token::Token;
-//use crate::data::object::Object;
-
-// TODO: this function is generate Object from SeparatedToken. The Object is contained by crate::data::object, and used to express program's process. evaluator will evaluate this Object.
-fn generate_object(separated_tokens: Vec<SeparatedToken>) {}
